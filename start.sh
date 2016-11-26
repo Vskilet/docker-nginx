4614c97569bd6ca577bef334fcd4bb074058f5bb
Paul TREHIOU <nyanloutre@users.noreply.github.com>
2016-11-26 14:58:09 +0100

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,2 +1,2 @@
-[ -f /etc/letsencrypt/dhparam.pem ] || openssl dhparam -out /etc/nginx/ssl/dhparam.pem 4096
+[ -f /etc/letsencrypt/dhparam.pem ] || openssl dhparam -out /etc/letsencrypt/dhparam.pem 4096
 nginx -g "daemon off;"
