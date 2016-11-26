98f3c9a83f2f5285bbc5768bd1bfc970afa57825
Paul TREHIOU <nyanloutre@users.noreply.github.com>
2016-11-26 14:56:54 +0100

    Mise a jour dossier DH

--- start.sh
+++ start.sh
@@ -1,3 +1,2 @@
-
-[ -f /etc/nginx/ssl/dhparam.pem ] || openssl dhparam -out /etc/nginx/ssl/dhparam.pem 4096
+[ -f /etc/letsencrypt/dhparam.pem ] || openssl dhparam -out /etc/nginx/ssl/dhparam.pem 4096
 nginx -g "daemon off;"
